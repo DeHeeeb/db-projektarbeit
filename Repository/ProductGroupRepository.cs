45eaf5edb99ac6f4f67982939fd56846876dd3f5
Lukas Heeb <lu.heeb@gmail.com>
2021-07-18 16:38:56 +0200

    added product and product group repositories

---
+++ Repository/ProductGroupRepository.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using db_projektarbeit.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace db_projektarbeit.Repository
+{
+    class ProductGroupRepository : RepositoryBase<ProductGroup>
+    {
+        public new List<ProductGroup> GetAll()
+        {
+            List<ProductGroup> productGroups = new List<ProductGroup>();
+            using var context = new ProjectContext();
+
+            var result = context.ProductGroups.FromSqlRaw(
+                ";WITH CTE_ProductGroup " +
+                "(Id, Name, ParentId, ProductLevel) " +
+                "AS (SELECT " +
+                            "Id," +
+                            "Name," +
+                            "ParentId," +
+                            "0 AS ProductLevel " +
+                "FROM dbo.ProductGroups " +
+                "WHERE ParentId IS NULL " +
+                "UNION ALL " +
+                "SELECT " +
+                            "pn.Id," +
+                            "pn.Name," +
+                            "pn.ParentId," +
+                            "p1.ProductLevel + 1 " +
+                "FROM dbo.ProductGroups AS pn " +
+                "INNER JOIN CTE_ProductGroup AS p1 " +
+                    "ON p1.Id = pn.ParentId " +
+                ") " +
+                "SELECT " +
+                            "Id," +
+                            "Name," +
+                            "ParentId," +
+                            "ProductLevel " +
+                "FROM CTE_ProductGroup " +
+                "ORDER BY ParentId;"
+                );
+
+            foreach (var item in result)
+            {
+                productGroups.Add(item);
+            }
+            return productGroups;
+        }
+    }
+}
