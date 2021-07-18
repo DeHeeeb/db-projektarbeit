845e1cdf7495444862db145a2906ab1f5578d1cc
Lukas Heeb <lu.heeb@gmail.com>
2021-07-18 16:13:00 +0200

    added repository interface and first implementation for cities

---
+++ Repository/IRepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace db_projektarbeit.Repository
+{
+    interface IRepositoryBase<M>
+    {
+        List<M> GetAll();
+
+        M Save(M entity);
+
+        M Update(M entity);
+
+        M Delete(int id);
+    }
+}
---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using db_projektarbeit.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace db_projektarbeit.Repository
+{
+    abstract class RepositoryBase<M> : IRepositoryBase<M> where M : class
+    {
+        public List<M> GetAll()
+        {
+            using var context = new ProjectContext();
+
+            var table = context.Set<M>();
+
+            return table.ToList();
+        }
+
+        public M Save(M entity)
+        {
+            using var context = new ProjectContext();
+
+            var table = context.Set<M>();
+            var attach = table.Attach(entity);
+            context.SaveChanges();
+
+            return attach.Entity;
+        }
+
+        public M Update(M entity)
+        {
+            using var context = new ProjectContext();
+
+            var table = context.Set<M>();
+            var attach = table.Attach(entity);
+            context.Entry(entity).State = EntityState.Modified;
+            context.SaveChanges();
+
+            return attach.Entity;
+        }
+
+        public M Delete(int id)
+        {
+            using var context = new ProjectContext();
+
+            var table = context.Set<M>();
+            var existing = table.Find(id);
+
+            try
+            {
+                table.Remove(existing);
+                context.SaveChanges();
+            }
+            catch
+            {
+                return null;
+            }
+
+            return existing;
+        }
+    }
+}
