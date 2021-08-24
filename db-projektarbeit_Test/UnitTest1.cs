a85ac7568fc2e9f9a23c31ed7593f319763268b6
marctraber <traber.marc@hispeed.ch>
2021-08-24 20:08:56 +0200

    Bugfix TestProject

---
+++ db-projektarbeit_Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using db_projektarbeit;
+using System;
+using Xunit;
+
+namespace db_projektarbeit_Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var ttt = new Customer();
+
+        }
+    }
+}
